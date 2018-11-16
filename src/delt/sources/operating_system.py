3669cefc7c3e17f1bbb4fae649e76e416cd9f4ac
Seth M. Larson <sethmichaellarson@gmail.com>
2018-11-16 18:36:49 +0000

    Update operating_system.py

--- src/delt/sources/operating_system.py
+++ src/delt/sources/operating_system.py
@@ -1,3 +1,5 @@
+import sys
+import platform
 import distro
 from ._base import DataSource
 
@@ -10,4 +12,8 @@ class OperatingSystemSource(DataSource):
         return True
 
     def get_values(self):
+        if sys.platform == "darwin":
+            return {"os.id": "macos", "os.version": platform.mac_ver()[0]}
+        elif sys.platform == "win32":
+            return {"os.id": "windows", "os.version": platform.win32_ver()[1]}
         return {"os.id": distro.id(), "os.version": distro.version(best=True)}
