a5bb4fcf118e95916982f0d24ddef0eea71c47fa
Seth Michael Larson <sethmichaellarson@gmail.com>
2018-11-15 22:53:36 -0600

    fix regex

--- src/delt/sources/brew.py
+++ src/delt/sources/brew.py
@@ -6,16 +6,16 @@ class BrewSource(DataSource):
     name = "brew"
     priority = DataSource.PRI_PM
 
-    brew_list_regex = re.compile(r"^([^\s]+)\s+([^\s+])$")
+    brew_list_regex = re.compile(r"^([^\s]+)\s+([^\s]+)$")
 
     def is_active(self):
         return self.context.get_returncode_from_popen("brew --version")
 
     def get_values(self):
-        apt_list = self.context.get_output_from_popen("brew list --full-name --versions")
+        brew_list = self.context.get_output_from_popen("brew list --full-name --versions")
         packages = {}
 
-        for line in apt_list.split("\n"):
+        for line in brew_list.split("\n"):
             match = self.brew_list_regex.match(line.strip())
             if match:
                 packages[match.group(1)] = match.group(2)
